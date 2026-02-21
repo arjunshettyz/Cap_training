4eb514840edaa1e3fe28acf2bd9b1a6b879cbe9e
Arjun Shetty <arjunpangunuri@gmail.com>
2026-02-21 15:23:21 +0530

    Re-push project

---
+++ adonet/AdoNet1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter Id: ");
+        int id = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter Name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter Marks: ");
+        int marks = int.Parse(Console.ReadLine());
+
+        string connectionString = "Data Source=.;Initial Catalog=StudentDB;Integrated Security=True";
+
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            string query = "INSERT INTO Student(Id, Name, Marks) VALUES(@Id, @Name, @Marks)";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            cmd.Parameters.AddWithValue("@Id", id);
+            cmd.Parameters.AddWithValue("@Name", name);
+            cmd.Parameters.AddWithValue("@Marks", marks);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            Console.WriteLine("Inserted Successfully");
+        }
+    }
+}
