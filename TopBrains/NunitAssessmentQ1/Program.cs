5a0c5e1f3fbfc8201e8972709b68312118ac50ac
Arjun Shetty <arjunpangunuri@gmail.com>
2026-02-21 10:54:08 +0530

    Re-push project

--- TopBrains/NunitAssessmentQ1/Program.cs
+++ TopBrains/NunitAssessmentQ1/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string[] parts = input.Split(' ');
+
+        double?[] values = new double?[parts.Length];
+
+        for (int i = 0; i < parts.Length; i++)
+        {
+            if (parts[i].ToLower() == "null")
+                values[i] = null;
+            else
+                values[i] = double.Parse(parts[i]);
+        }
+
+        double? avg = GetAverage(values);
+
+        if (avg == null)
+            Console.WriteLine("null");
+        else
+            Console.WriteLine(avg.Value.ToString("F2"));
+    }
+
+    static double? GetAverage(double?[] values)
+    {
+        double sum = 0;
+        int count = 0;
+
+        foreach (double? v in values)
+        {
+            if (v.HasValue)
+            {
+                sum += v.Value;
+                count++;
+            }
+        }
+
+        if (count == 0)
+            return null;
+
+        return Math.Round(sum / count, 2, MidpointRounding.AwayFromZero);
+    }
+}
